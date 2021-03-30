b344c82ae6ff57f55f1960542efa76c7dd56f67b
Rodrigo Gomes <cklrod@hotmail.com>
2021-03-30 10:35:29 -0300

    Adicionando arquivo index.html e main.js

---
+++ js/main.js
@@ -0,0 +1,62 @@
+//var nome = "Rodrigo Gomes";
+//var idade = 29;
+//var frase = "Japão é o melhor time do mundo!";
+//alert("Bem vindo " + nome);
+//alert(nome + " tem " + idade + " anos.");
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+
+//Trabalhando com array
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista[2]);
+//alert(lista[2]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" x "));
+
+//Trabalhando com dicionário.
+/*var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);*/
+
+/*var frutas = [
+    { nome: "maça", cor: "vermelha" },
+    { nome: "pêra", cor: "verde" },
+    { nome: "abacaxi", cor: "amarelo" }
+];
+console.log(frutas);
+alert(frutas[1].nome);*/
+
+//estrura de controles
+
+/*var idade = prompt("Qual é a sua idade?");
+if (idade => 18) {
+    alert("Maior de idade");
+} else {
+    alert("Menor de idade!")
+};*/
+
+/*var count = 0;
+
+while (count < 5) { 
+    console.log(count);
+    count ++;
+};*/
+
+/*var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+};
+*/
+
+//trabalhando com datas
+
+var d  = new Date();
+//alert(d);
+alert(d.getDate());
